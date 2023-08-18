8235f7f08591d12c931fccc96e38f3a6c3d40dd1
sanket-s10 <sanketsangar.11@gmail.com>
2023-08-18 23:40:10 +0530

    Added side bar component and body

--- app/common/header.tsx
+++ app/common/header.tsx
@@ -2,7 +2,7 @@
 const Header = () => {
 
 return (<header>
-    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 ml-64">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <div className="flex items-center lg:order-2">
                 <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./common/header";
+import Sidebar from "./common/sidebar/sidebar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
+    <main className="flex min-h-screen flex-col justify-between p-4">
         <Header />
+        <Sidebar />
     </main>
   )
 }
