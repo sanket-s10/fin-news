de000cef21f750903009b1fe213787211d06a48a
sanket-s10 <sanketsangar.11@gmail.com>
2023-08-23 04:02:09 +0530

    Added right top stories section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Header from "./common/header";
 import Sidebar from "./common/sidebar/sidebar";
 import NewsLanding from "./components/NewsLanding/NewsLanding";
+import TopStories from "./components/TopStories/TopStories";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-4">
+    <main className="flex min-h-screen flex-col p-4">
         <Header />
         <Sidebar />
-        <NewsLanding/>
+        <div className="flex p-4 gap-4">
+          <NewsLanding/>
+          <TopStories />
+        </div>
     </main>
   )
 }
